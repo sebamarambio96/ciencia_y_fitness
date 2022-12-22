d85f87df09f159c2cdd33d687fe6984f112c36cb
Sebastian Marambio <s.marambiosandoval@gmail.com>
2022-12-21 23:49:48 -0300

    Se agregan nueva funcionalidad de registro de usuarios en clases y eliminacion de datos

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,6 @@ Ejercicio moderado (3 - 5 días por semana) = TMB x 1,55.
 Ejercicio fuerte (6 - 7 días por semana) = TMB x 1,725.
 Ejercicio muy fuerte (dos veces al día, entrenamientos muy duros) = TMB x 1,9. */
 
-let nombre;
 let sexo;
 let peso;
 let altura;
@@ -20,6 +19,26 @@ let edad;
 let tmb;
 let tmbFinal;
 let ejercicio;
+let solicitantes = [{id:0}]
+
+class Solicitante {
+    constructor(peso, altura, edad, ejercicio, id, sexo) {
+        this.peso = peso
+        this.altura = altura
+        this.edad = edad
+        this.ejercicio = ejercicio
+        this.id = id
+        this.sexo = sexo
+    }
+
+    mostrarInfo(){
+        const hoy = new Date()
+        return `Datos solicitante N°${this.id} :\n- Sexo: ${this.sexo}\n- Peso: ${this.peso}\n- Altura: ${this.edad}\n- Edad: ${this.edad}\n - Ejercicio: ${this.ejercicio}\n- Última evaluación: ${hoy.toDateString()}`
+    }
+
+}
+
+
 
 function datos() {
     peso = prompt('Ingrese su peso en kilogramos:')
@@ -85,7 +104,35 @@ for (let x = 1; x <= participantes; x += 1) {
     }
     console.log(tmbFinal)
 
-    window.alert(`La TMB del solicitante ${x} es: ${tmb} \nPara su nivel de ejercicio su gasto diario es ${tmbFinal}. `)
+    window.alert(`La TMB del solicitante ${x} es: ${Math.round(tmb)} \nPara su nivel de ejercicio su gasto diario es: ${Math.round(tmbFinal)} `)
     console.log(x)
-}
 
+    solicitantes[x] = new Solicitante(peso, altura, edad, ejercicio, x, sexo)
+}
+console.log(solicitantes[1].mostrarInfo());
+
+console.log(solicitantes);
+
+let condicion = true
+while (condicion === true) {
+    let solicitud = parseInt(prompt('1. Obtener más detalles de un solicitante.\n2. Eliminar datos de un solicitante.\n3. Terminar consulta.'))
+    let listaSoli = ''
+    for (x in solicitantes) {
+        if (solicitantes[x].id === 0){
+            continue
+        }
+        let soli = `- ${solicitantes[x].id}\n`
+        listaSoli += soli
+    }
+    if(solicitud === 1){
+        let s = prompt(`Ingrese el número del solicitante para obtener más detalles:\n${listaSoli}`)        
+        window.alert(solicitantes[s].mostrarInfo())
+    } else if (solicitud === 2) {
+        let n = prompt(`Ingrese el número del solicitante que desea eliminar:\n${listaSoli}`)
+        solicitantes = solicitantes.filter(solicitante => solicitante.id != n)
+    } else if (solicitud === 3) {
+        condicion = false
+    } else{
+        window.alert('Ingrese una opción válida')
+    }
+}
